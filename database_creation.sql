e8bd8a47eae72aa58a1b9d276ad061925a89dbcd
Hiram <hmunn@uw.edu>
2016-04-13 17:10:47 -0700

    Add AUTO_INCREMENT

--- database_creation.sql
+++ database_creation.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tblHOUSE (
-	house_id INT NOT NULL,
+	house_id INT NOT NULL AUTO_INCREMENT,
 	house_name VARCHAR(30) NOT NULL,
 	rent_total INT,
 	PRIMARY KEY(house_id)
 );
 
 CREATE TABLE tblUSER (
-	user_id INT NOT NULL,
+	user_id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(30) NOT NULL,
 	phone_num VARCHAR(30) NOT NULL,
 	email VARCHAR(60) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE tblUSER (
 );
 
 CREATE TABLE tblHOUSE_USER (
-	house_user_id INT NOT NULL,
+	house_user_id INT NOT NULL AUTO_INCREMENT,
 	house_id INT NOT NULL,
 	user_id INT NOT NULL,
 	PRIMARY KEY (house_user_id),
@@ -23,14 +23,14 @@ CREATE TABLE tblHOUSE_USER (
 );
 
 CREATE TABLE tblGROUP (
-	group_id INT NOT NULL,
+	group_id INT NOT NULL AUTO_INCREMENT,
 	group_name VARCHAR(30) NOT NULL,
 	group_descr VARCHAR(30) NOT NULL,
 	PRIMARY KEY (group_id)
 );
 
 CREATE TABLE tblUSER_GROUPS (
-	user_group_id INT NOT NULL,
+	user_group_id INT NOT NULL AUTO_INCREMENT,
 	user_id INT NOT NULL,
 	group_id INT NOT NULL,
 	PRIMARY KEY (user_group_id),
@@ -39,7 +39,7 @@ CREATE TABLE tblUSER_GROUPS (
 );
 
 CREATE TABLE tblTASK (
-	task_id INT NOT NULL,
+	task_id INT NOT NULL AUTO_INCREMENT,
 	task_name VARCHAR(30) NOT NULL,
 	task_descr VARCHAR(500),
 	task_time TIME NOT NULL,
@@ -51,21 +51,21 @@ CREATE TABLE tblTASK (
 );
 
 CREATE TABLE tblBILL (
-	task_id INT NOT NULL,
+	task_id INT NOT NULL AUTO_INCREMENT,
 	bill_amount INT NOT NULL,
 	PRIMARY KEY (task_id),
 	FOREIGN KEY (task_id) REFERENCES tblTASK(task_id)
 );
 
 CREATE TABLE tblCHORE (
-	task_id INT NOT NULL,
+	task_id INT NOT NULL AUTO_INCREMENT,
 	priority VARCHAR(30) NOT NULL,
 	PRIMARY KEY (task_id),
 	FOREIGN KEY (task_id) REFERENCES tblTASK(task_id)
 );
 
 CREATE TABLE tblUSER_TASK (
-	user_task_id INT NOT NULL,
+	user_task_id INT NOT NULL AUTO_INCREMENT,
 	task_id INT NOT NULL,
 	task_owner_id INT NOT NULL,
 	task_creator_id INT NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE tblUSER_TASK (
 );
 
 CREATE TABLE tblCOMMENT (
-	comment_id INT NOT NULL,
+	comment_id INT NOT NULL AUTO_INCREMENT,
 	comment_descr VARCHAR(30) NOT NULL,
 	comment_time TIMESTAMP NOT NULL,
 	comment_owner_id INT NOT NULL,
@@ -85,7 +85,7 @@ CREATE TABLE tblCOMMENT (
 );
 
 CREATE TABLE tblUSER_TASK_COMMENT (
-	user_task_comment_id INT NOT NULL,
+	user_task_comment_id INT NOT NULL AUTO_INCREMENT,
 	user_task_id INT NOT NULL,
 	comment_id INT NOT NULL,
 	PRIMARY KEY (user_task_comment_id),
